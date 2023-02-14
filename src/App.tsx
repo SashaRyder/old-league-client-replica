2d2101144f18001a0bde7c9c1f772731c5d508f8
Sasha Ryder <8694395+SashaRyder@users.noreply.github.com>
2023-02-14 00:16:10 +0000

    Started base project for LoL OG client replica

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import { AppRoot } from "./app.styled";
+
+const App = () => {
+  return (
+    <AppRoot></AppRoot>
+  )
+}
+
+export default App;
---
+++ src/app.styled.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const AppRoot = styled.div`
+  background-color: black;
+  width: 100vw;
+  height: 100vh;
+`;
